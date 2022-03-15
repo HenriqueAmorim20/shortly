9b344bc21d270578af7a8ffb47643a30cb2e45ed
Henrique Melo <hacmelo@gmail.com>
2022-03-15 17:27:08 -0300

    global css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="app">
     <v-main>
       <Nuxt />
     </v-main>
@@ -11,3 +11,30 @@ export default {
   name: "DefaultLayout",
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap');
+
+:root {
+  /* ### Primary */
+
+  --Cyan: hsl(180, 66%, 49%);
+  --DarkViolet: hsl(257, 27%, 26%);
+
+  /* ### Secondary */
+
+  --Red: hsl(0, 87%, 67%);
+
+  /* ### Neutral */
+
+  --Gray: hsl(0, 0%, 75%);
+  --GrayishViolet: hsl(257, 7%, 63%);
+  --VeryDarkBlue: hsl(255, 11%, 22%);
+  --VeryDarkViolet: hsl(260, 8%, 14%);
+}
+
+.app {
+  background-color: #fff !important;
+  color: var(--DarkViolet) !important;
+}
+</style>
